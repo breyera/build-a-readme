677f2a4c2e1e24175f477035c18b8940b23221c3
breyera <alicia_breyer@yahoo.com>
2021-04-02 19:35:31 -0700

    added basic inquirer info and example readme

---
+++ index.js
@@ -0,0 +1,41 @@
+var inquirer = require('inquirer');
+
+let readMeInfo = async () => {
+    const answers = await inquirer.prompt([
+        {
+            type: 'input',
+            message: ,
+            name: 
+        },
+        {
+            type: 'input',
+            message: ,
+            name: 
+        },
+        {
+            type: ,
+            message: ,
+            name: 
+        },
+        {
+            type: 'input',
+            message: ,
+            name: 
+        },
+        {
+            type: 'input',
+            message: ,
+            name: 
+        },
+        {
+            type: 'input',
+            message: ,
+            name: 
+        },
+        {
+            type: 'input',
+            message: ,
+            name: 
+        }
+    ])
+}
