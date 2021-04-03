4caeff6bd236e6b6de55bec00a3241bc25ae6703
breyera <alicia_breyer@yahoo.com>
2021-04-02 20:34:01 -0700

    added readme template and write file function

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # build-a-readme
-use node.js to build a readme based on prompts
+
+![Badge for GitHub repo top language](https://img.shields.io/github/languages/top/breyera/build-a-readme?style=flat&logo=appveyor) ![Badge for GitHub last commit](https://img.shields.io/github/last-commit/breyera/build-a-readme?style=flat&logo=appveyor)
--- index.js
+++ index.js
@@ -1,41 +1,120 @@
 var inquirer = require('inquirer');
 
 let readMeInfo = async () => {
+    // create variable for answers 
     const answers = await inquirer.prompt([
+        //ask questions for users to input information using inquirer
         {
             type: 'input',
-            message: ,
-            name: 
+            message: 'What is the name of your project?',
+            name: 'projectName'
         },
         {
             type: 'input',
-            message: ,
-            name: 
-        },
-        {
-            type: ,
-            message: ,
-            name: 
+            message: 'Enter a brief description of your project: ',
+            name: 'description'
         },
         {
             type: 'input',
-            message: ,
-            name: 
+            message: 'If applicable, describe the steps required to install your project.',
+            name: 'installation'
         },
         {
             type: 'input',
-            message: ,
-            name: 
+            message: 'How will this project be used? Provide instructions and examples of your project.',
+            name: 'usage'
+        },
+        {
+            type: 'list',
+            message: 'Chose the appropriate license for this project: ',
+            name: 'license',
+            choices: ['Apache', 'Academic', 'GNU', 'ISC', 'MIT', 'Mozilla', 'Open']
         },
         {
             type: 'input',
-            message: ,
-            name: 
+            message: 'Who contributed to this project?',
+            name: 'contributing'
         },
         {
             type: 'input',
-            message: ,
-            name: 
+            message: 'Is there a test included?',
+            name: 'tests'
+        },
+        {
+            type: 'input',
+            message: 'What do I do if I have an issue?',
+            name: 'questions'
+        },
+        {
+            type: 'input',
+            message: 'Please enter your GitHub username.',
+            name: 'username'
+        },
+        {
+            type: 'input',
+            message: 'Please enter your email address.',
+            name: 'email'
         }
     ])
+    //user answers to fill a readme page
+    const { projectName, description, installation, usage, license, contributing, tests, questions, username, email } = answers;
+    const readMePage = `<h1>${ projectName }</h1>
+
+    ![badge](https://img.shields.io/badge/license-${ license }-brightgreen)
+    <br />
+    
+    ## Description
+    
+    🔍 ${ description }
+    
+    ## Table of Contents
+    
+    - [Description](#description)
+    - [Installation](#installation)
+    - [Usage](#usage)
+    - [License](#license)
+    - [Contributing](#contributing)
+    - [Tests](#tests)
+    - [Questions](#questions)
+    
+    ## Installation
+    
+    💾 ${ installation }
+    
+    ## Usage
+    
+    💻 ${ usage }
+    
+    ## License
+    
+    ![badge](https://img.shields.io/badge/license-${ license }-brightgreen)
+    <br />
+    This application is covered by the ${ license } license.
+    
+    ## Contributing
+    
+    👪 ${ contributing }
+    
+    ## Tests
+    
+    ✏️ ${ tests }
+    
+    ## Questions
+    
+    ${ questions }<br />
+    <br />
+    Find me on GitHub: [${ username }](https://github.com/${ username })<br />
+    <br />
+    Email me with any questions: ${ email }<br />`;
+    //write user inputs to a new readme file
+    const fs = require('fs');
+        fs.writeFile('./example-readme/README.md', readMePage, (error) => {
+            if (error) {
+                console.log(error);
+            } else {
+                console.log('Stored Successfully');
+            }
+        });
 }
+//call function and create readme
+readMeInfo();
